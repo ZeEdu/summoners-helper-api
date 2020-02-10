cb8519e03288c52e72816b09f118101f137744c8
Eduardo <eduardo.carlos.siqueira@gmail.com>
2020-02-10 17:56:29 -0300

    First Route Added

--- api/routes/validations.js
+++ api/routes/validations.js
@@ -1,38 +1,19 @@
 require('dotenv').config();
-const express = require('express');
 const mariadb = require('mariadb');
+const express = require('express');
 
 const router = express.Router();
 
 router.get('/isusernametaken/:username', (req, res, next) => {
-    mariadb
-        .createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            port: process.env.DB_PORT,
-            database: process.env.DB_DATABASE
-        })
-        .then(conn => {
-            conn.query('SELECT USERNAME FROM USERPROFILE WHERE USERNAME = (?)', [
-                req.params.username
-            ])
-                .then(rows => {
-                    res.status(200).json(rows);
-                    // res.status(200).json(rows);
-                    conn.end();
-                })
-                .catch(err => {
-                    res.send(err);
-                });
-        })
-        .catch(err => {
-            res.send(err);
-        });
-    // next();
+    try {
+        const user = req.params.username;
+        res.status(200).json({ message: user });
+    } catch (err) {
+        res.status(202).send(err);
+    }
 });
 
-module.exports = router;
+// router.get('/isusernametaken/:username', (req, res, next) => {
 //     mariadb
 //         .createConnection({
 //             host: process.env.DB_HOST,
@@ -42,10 +23,17 @@ module.exports = router;
 //             database: process.env.DB_DATABASE
 //         })
 //         .then(conn => {
-//             conn.query('SELECT * FROM USERPROFILE WHERE USERNAME = (?)', [req.params.username])
+//             conn.query('SELECT USERNAME FROM USERPROFILE WHERE USERNAME = (?)', [
+//                 req.params.username
+//             ])
 //                 .then(rows => {
-//                     res.send(rows);
 //                     console.log(rows);
+//                     // console.log(typeof rows);
+//                     // const entries = Object.entries(rows);
+//                     // const popped = entries.pop();
+//                     // console.log(`Popped: ${popped}`);
+//                     // console.log(entries);
+//                     res.status(200).json(rows);
 //                     conn.end();
 //                 })
 //                 .catch(err => {
@@ -55,3 +43,6 @@ module.exports = router;
 //         .catch(err => {
 //             res.send(err);
 //         });
+// });
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,18 +1,68 @@
+require('dotenv').config();
+const mariadb = require('mariadb');
 const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const validationRoutes = require('./api/routes/validations');
+// const validationRoutes = require('./api/routes/validations');
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+    'capacitor://localhost',
+    'ionic://localhost',
+    'http://localhost',
+    'http://localhost:8080',
+    'http://localhost:8100'
+];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Origin not allowed by CORS'));
+        }
+    }
+};
+
+app.options('*', cors(corsOptions));
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/validations', validationRoutes);
+app.get('/validations/isusernametaken/:username', cors(), (req, res, next) => {
+    mariadb
+        .createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            port: process.env.DB_PORT,
+            database: process.env.DB_DATABASE
+        })
+        .then(conn => {
+            conn.query('SELECT USERNAME FROM USERPROFILE WHERE USERNAME = (?)', [
+                req.params.username
+            ])
+                .then(rows => {
+                    console.log(rows);
+                    // console.log(typeof rows);
+                    // const entries = Object.entries(rows);
+                    // const popped = entries.pop();
+                    // console.log(`Popped: ${popped}`);
+                    // console.log(entries);
+                    res.status(200).json(rows);
+                    conn.end();
+                })
+                .catch(err => {
+                    res.send(err);
+                });
+        })
+        .catch(err => {
+            res.send(err);
+        });
+});
 
 // Handling Errors
 app.use((res, req, next) => {
@@ -33,101 +83,3 @@ app.use((error, req, res, next) => {
 
 // Exporting App
 module.exports = app;
-
-// app.use((req, res) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content-Type, Authorization'
-//     );
-//     if (req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, DELETE, GET');
-//         return res.status(200).json({});
-//     }
-//     console.log('not getting here');
-// });
-
-// const whitelist = [
-// 'capacitor://localhost',
-// 'ionic://localhost',
-// 'http://localhost',
-// 'http://localhost:8080',
-// 'http://localhost:3306',
-// 'http://localhost:8100',
-// 'http://localhost:4200'
-// ];
-
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         if (whitelist.indexOf(origin) !== -1) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     }
-// };
-
-// app.options('*', cors(corsOptions));
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(express.json());
-
-// app.get('/', (req, res) => {
-//     res.json({ status: 'Sever is Running' });
-// });
-
-// app.get('/validation/isusernametaken/:username', cors(corsOptions), (req, res) => {
-//     // Start mariadb connection
-//     mariadb
-//         .createConnection({
-//             host: process.env.DB_HOST,
-//             user: process.env.DB_USER,
-//             password: process.env.DB_PASS,
-//             port: process.env.DB_PORT,
-//             database: process.env.DB_DATABASE
-//         })
-//         .then(conn => {
-//             conn.query('SELECT * FROM USERPROFILE WHERE USERNAME = (?)', [req.params.username])
-//                 .then(rows => {
-//                     res.send(rows);
-//                     console.log(rows);
-//                     conn.end();
-//                 })
-//                 .catch(err => {
-//                     res.send(err);
-//                 });
-//         })
-//         .catch(err => {
-//             res.send(err);
-//         });
-// });
-
-// app.get('/validation/isemailtaken/:email', (req, res) => {
-//     // Start mariadb connection
-//     mariadb
-//         .createConnection({
-//             host: process.env.DB_HOST,
-//             user: process.env.DB_USER,
-//             password: process.env.DB_PASS,
-//             port: process.env.DB_PORT,
-//             database: process.env.DB_DATABASE
-//         })
-//         .then(conn => {
-//             conn.query('SELECT * FROM USERPROFILE WHERE EMAIL = (?)', [req.params.email])
-//                 .then(rows => {
-//                     res.send(rows);
-//                     conn.end();
-//                 })
-//                 .catch(err => {
-//                     res.send(err);
-//                 });
-//         })
-//         .catch(err => {
-//             res.send(err);
-//         });
-// });
-
-// app.listen(port, () => {
-//     console.log(`Running on port ${port}`);
-// });
