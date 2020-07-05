c92b7a20f85f0a7e458f38964255bb12bd58fefd
eduardo_Siqueira <eduardo.carlos.siqueira@gmail.com>
2020-07-05 16:14:31 -0300

    Fixes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const helmet = require('helmet');
 
 const checkIfAuthenticated = require('./middlewares/auth.middleware');
 
@@ -14,9 +13,9 @@ const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.use('/api/v1/validations/', validations);
 app.use('/api/v1/users/', users);
