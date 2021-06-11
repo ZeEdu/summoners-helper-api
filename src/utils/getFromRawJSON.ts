b3b6aee5ea496db633c3f3b611249dccfef59cfb
eduardo_Siqueira <eduardo.carlos.siqueira@gmail.com>
2021-06-11 10:53:54 -0300

    Paths

--- src/utils/getFromRawJSON.ts
+++ src/utils/getFromRawJSON.ts
@@ -3,7 +3,10 @@ import path from 'path';
 
 const getFromRawJSON = (file: string) => {
    const rawItemsJSON = fs.readFileSync(
-      path.join(__dirname.split('src')[0], `public/10.7.1/data/en_US/${file}.json`),
+      path.join(
+         __dirname.split('src')[0].split('dist')[0],
+         `public/10.7.1/data/en_US/${file}.json`
+      ),
       'utf8'
    );
    return JSON.parse(rawItemsJSON);
