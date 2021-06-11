1ad11a0bba7375252aa0ab9226780f927b7b4fc7
eduardo_Siqueira <eduardo.carlos.siqueira@gmail.com>
2021-06-11 11:00:04 -0300

    pathing

--- src/utils/getFromRawJSON.ts
+++ src/utils/getFromRawJSON.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const getFromRawJSON = (file: string) => {
    const rawItemsJSON = fs.readFileSync(
       path.join(
-         __dirname.split('src')[0].split('dist')[0],
+         __dirname.split('src')[0].split('utils')[0],
          `public/10.7.1/data/en_US/${file}.json`
       ),
       'utf8'
